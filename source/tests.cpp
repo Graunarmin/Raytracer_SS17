8d65dd35d5f8dcf73ca23109c3ca938e9d243b38
Johanna Sacher <johanna.sacher@uni-weimar.de>
2017-06-13 12:43:30 +0200

    Add first test, working

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,7 +1,15 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
+#include "shape.hpp"
+#include "sphere.hpp"
+#include "box.hpp"
 
-TEST_CASE("";"[]")
+TEST_CASE("Box Default Constructor", "[5.2 Box Constructor]"){
+  Box box{};
+  REQUIRE(box.getMin().x == -1);
+  REQUIRE(box.getMin().y == -1);
+  REQUIRE(box.getMin().z == -1);
+}
 
 int main(int argc, char *argv[])
 {
