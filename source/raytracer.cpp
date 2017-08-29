6fc5c8fc60f0d4f6322ef52315f33bfb109f409f
Valerie Lemuth <valerie.lemuth@uni-weimar.de>
2017-08-29 17:37:04 +0200

    Add some things....

--- source/raytracer.cpp
+++ source/raytracer.cpp
@@ -8,26 +8,16 @@
 
 int main(int argc, char* argv[])
 {
-  // unsigned const width = 400;
-  // unsigned const height = 400;
-
-  //std::string const filename = "./Szene.ppm";
   //std::string const filepath = "../../doc/szene.txt";
 
-  //std::string const filename = "./Szene1.ppm";
   std::string const filepath = "../../doc/szene1.txt";
 
-  //std::string const filename = "./Szene2.ppm";
   //std::string const filepath = "../../doc/szene2.txt";
 
-  //filename als instream anfragen
   Scene szene{};
   Scene superSzene = szene.SDFloader(filepath);
   superSzene.printScene();
 
-
-  //glm::vec3 beobachter{0.0f};
-  //beobachter/camera später in scene!!
   glm::vec3 screenCenter{0.0f, 0.0f, -100.0f};
 
   Renderer app{superSzene, screenCenter};
